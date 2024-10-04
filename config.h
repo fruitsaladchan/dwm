ba670fcc63a75f26a1f501007486908067e500a2
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-10-04 19:51:27 -0400

    Add files via upload

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  = 1;       /* border pixel of windows */
+static const unsigned int snap      = 32;      /* snap pixel */
+static const int showbar            = 1;       /* 0 means no bar */
+static const int topbar             = 1;       /* 0 means bottom bar */
+static const int swallowfloating   = 0;       /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 9;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 9;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 9;       /* horiz outer gap between windows and screen edge */
@@ -38,9 +39,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "firefox",   		    NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
@@ -95,7 +96,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_space, zoom,           {0} },
+   	{ MODKEY,			            XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
--- config.mk
+++ config.mk
@@ -6,6 +6,7 @@ VERSION = 6.5
 # paths
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
+KVMLIB = -lkvm
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
@@ -23,7 +24,7 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender -lX11-xcb -lxcb -lxcb-res ${KVMLIB}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
