0305df260ef3bffe3675f1bf5eb753505a284bdc
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-10-04 20:15:58 -0400

    Add files via upload

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static const unsigned int borderpx  = 1;       /* border pixel of windows */
 static const unsigned int snap      = 32;      /* snap pixel */
 static const int showbar            = 1;       /* 0 means no bar */
 static const int topbar             = 1;       /* 0 means bottom bar */
-static const int swallowfloating   = 0;       /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 0;       /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 9;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 9;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 9;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 9;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "UbuntuMono Nerd Font:pixelsize=11:antialias=true"};
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "UbuntuMono Nerd Font:pixelsize=10:antialias=true"};
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray3[]       = "#f1be9b"; 
 static const char col_gray1[]       = "#020914";
 static const char col_gray2[]       = "#a8856c";
@@ -48,6 +48,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static int attachbelow = 1;    /* 1 means attach at the end */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #include "vanitygaps.c"
@@ -98,7 +99,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
    	{ MODKEY,			            XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY,                       XK_Tab,    toggleAttachBelow,  {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
