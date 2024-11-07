3b1b29820309f5ac213cec50fdc391fa2b34eb69
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-11-07 10:49:45 -0500

    rename pic.sh to dmenu-wallpaper.sh

--- scripts/dmenu-wallpaper.sh
+++ scripts/dmenu-wallpaper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Define the directory containing the pictures
 PICTURES_DIR=~/Pictures
 
 # Find all image files in the directory
 IMAGE=$(find "$PICTURES_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.bmp" \) | dmenu -i -l 10)
 
-# If an image was selected, set it as the wallpaper using setbg
 if [ -n "$IMAGE" ]; then
   setbg "$IMAGE"
   echo "Wallpaper set to $IMAGE"
