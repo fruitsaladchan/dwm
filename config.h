50f14ce9623a7ef14e45db23a6bf3feccbe91653
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-11-26 00:30:06 -0500

    Add files via upload

--- config.h
+++ config.h
@@ -136,7 +136,6 @@ static const Key keys[] = {
 };
 
 static const Button buttons[] = {
-    /* click                event mask      button          function argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
